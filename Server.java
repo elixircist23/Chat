fbf35dd194cff4292662d8ac2abdfc21f024a698
elixircist23 <elixircist@gmail.com>
2016-05-20 19:50:50 -0400

    new implementation

--- Server.java
+++ Server.java
@@ -2,102 +2,103 @@ import java.net.*;
 import java.io.*;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 
+//SERVER KEEPS LIST OF THREADS, ID'S THEM AND IF CLIENT QUITS, IT SENDS QUIT OBJECT
+//AND SERVER ERASES IT FROM LIST.
 
-public class Server extends Thread{
+public class Server{
 	
-	private ServerSocket serverSocket;
-	private static Map<String, SocketAddress> users = new HashMap<String, SocketAddress>();
-	
-	//test take out later
-	users.put("ali", (SocketAddress)"127.0.0.1");
-
-	//returns string of class type, given an arbitrary object
-	public static String getObjType(Object obj){
-		return(obj.getClass().getTypeName());
+	public static void main(String[] args) throws Exception{
+		int port = 1500;
+		Server serverSocket = new Server(port);
+		serverSocket.StartServer();
 	}
 	
-	//check if user is in the arraylist users, add user, (later on delete etc)
-	public static boolean userFunctions(Object obj, SocketAddress address){
-		//grab the username from the object
-		String username = ((Username) obj).getUsername();
+	ServerSocket server = null;
+	Socket clientSocket = null;
+	int port;
+	
+	public Server(int port){
+		this.port = port;
+	}
+	
+	public void stopServer(){
+		System.out.println("Server shutting down");
+		System.exit(0);
+	}
+	
+	public void StartServer(){
+		try{
+			server = new ServerSocket(port);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 		
-		//is the given username a part of the dictionary?
-		//	if not, add it with its address
-		System.out.println("Checking username...");
-		if(!users.containsKey(username)){
-			users.put(username, address);
-			System.out.println("Creating new user...");
-			return(true);
-		}
-		//if it is, just return true (FOR NOW)
-		else{
-			System.out.println("Created new user " + username + " address " + address.toString());
-			return(true);
-		}
-
-	}
-	
-	public Server(int port) throws IOException{
-		serverSocket = new ServerSocket(port);
-		serverSocket.setSoTimeout(100000);
-	}
-	
-	//main method, to run socket and keep it alive
-	public void run(){
+		System.out.println("Waiting for connection");
 		
 		while(true){
-			
 			try{
-				//make a socket and accept connections from clients
-				Socket server = serverSocket.accept();
-				
-				System.out.println("Connected to " + server.getRemoteSocketAddress().toString());
-
-				//create object to later on help us distinguish received objects
-				Object obj = null;
-				
-				//input received will always be an object
-				InputStream inputStream = server.getInputStream();
-				ObjectInputStream objectInStream = new ObjectInputStream(inputStream);
-				
-				//try to read objects type
-				try{
-					obj = objectInStream.readObject();
-				}
-				catch(ClassNotFoundException c){
-					c.printStackTrace();
-				}
-
-				System.out.println("Got an object");
-				
-				if(getObjType(obj).equals("Username")){
-					userFunctions(obj, server.getRemoteSocketAddress());
-				}
-
-				System.out.println(users.containsKey("ali"));
+				clientSocket = server.accept();
+				ClientWorker clientThread = new ClientWorker(clientSocket, this);
+				clientThread.run();
 				
 			}catch(IOException e){
 				e.printStackTrace();
 			}
-			
 		}
+	}
+}
+
+class ClientWorker extends Thread{
+	Server server;
+	Socket clientSocket;
+	BufferedReader in = null;
+	PrintStream out = null;
+	
+	public ClientWorker(Socket clientSocket, Server server){
+		this.clientSocket = clientSocket;
+		this.server = server;
 		
-			
+		System.out.println("Connected to address: " + clientSocket.getInetAddress().getHostAddress());
+		
+		try{
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			out = new PrintStream(clientSocket.getOutputStream());
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 	
-	public static void main(String[] args){
+	public void run(){
+		String line;
 		
-		int port = 1500;
 		try{
-			Thread t = new Server(port);
-			t.start();
+			boolean serverStop = false;
+			
+			while(true){
+				line = in.readLine();
+				System.out.println("Client: " + line);
+				int n = Integer.parseInt(line);
+				
+				if(n == -1){
+					serverStop = true;
+					break;
+				}
+				
+				if(n == 0) break;
+				out.println("" + n*n);
+			}
+			
+			System.out.println("Connected ended");
+			in.close();
+			out.close();
+			clientSocket.close();
+			
+			if(serverStop) server.stopServer();
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 		
 	}
-	
 }
-	
-
