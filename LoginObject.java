158d6620efb3b6fa5d624bab74c3fea721001e1a
tbone255 <taha.amir13971@gmail.com>
2016-05-20 20:49:44 -0400

    login object basic

---
+++ LoginObject.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+
+public class LoginObject implements Serializable
+{
+	private String username;
+	private String password;
+
+	private static final long serialVersionUID = 1L;
+	
+	public LoginObject(String usr)
+	{
+		username = usr;
+	}
+	
+	public String toString()
+	{
+		return username;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+}
