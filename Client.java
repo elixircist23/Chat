11b5d6f62d63bdc1c55b05bbd264f03697cc6fd6
elixircist23 <elixircist@gmail.com>
2016-05-21 18:29:14 -0400

    object out and in works
    probably not going to bother with switches

--- Client.java
+++ Client.java
@@ -9,43 +9,57 @@ public class Client{
 		int port = 1500;
 		
 		Socket clientSocket = null;
-		//InputStream in = null;
-		//ObjectInputStream objectInStream = null;
+		InputStream in = null;
+		ObjectInputStream objectInStream = null;
 		OutputStream out = null;
 		ObjectOutputStream objectOutStream = null;
 		
 		try{
 			clientSocket = new Socket(hostname, port);
-			//in = clientSocket.getInputStream();
-			//objectInStream = new ObjectInputStream(in);
+			in = clientSocket.getInputStream();
 			out = clientSocket.getOutputStream();
-			objectOutStream = new ObjectOutputStream(out);
 			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		
-		//if(objectInStream == null){
-			//System.out.println("error");
-			//return;
-		//}
-		
+			
 		try{
 			while(true){
 				
-				System.out.println("hello");
-				LoginObject login = new LoginObject("ali");
-				objectOutStream.writeObject(login);
+				System.out.println("in client loop");
 				
-				Quit q = new Quit();
-				objectOutStream.writeObject(q);
+				objectOutStream = new ObjectOutputStream(out);
 				
-				break;
+				Scanner stdin = new Scanner(System.in);
+				String userIn = stdin.next();
+				
+				switch(userIn){
+					case "m":
+						TempMessage tmp = new TempMessage("hello");
+						objectOutStream.writeObject(tmp);
+						break;
+				
+					case "q":
+						Quit q = new Quit();
+						objectOutStream.writeObject(q);
+						break;
+				}
+				
+				if(userIn.equals("q")){
+					break;
+				}
+				
+				
+				objectInStream = new ObjectInputStream(in);
+				System.out.println(objectInStream.readObject());
+							
 				
 			}
 			
+			in.close();
+			out.close();
+			objectInStream.close();
 			objectOutStream.close();
-			//objectInStream.close();
 			clientSocket.close();
 			
 		}catch(Exception e){
