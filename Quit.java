9bbaf7acb4b0369ff6ec3410973cef1e4c15d1b6
elixircist23 <elixircist@gmail.com>
2016-05-22 19:37:47 -0400

    quit object

---
+++ Quit.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+public class Quit implements Serializable {
+
+	private static final long serialVersionUID = -1411571583156792990L;
+	boolean q;
+	public Quit(){
+		q = true;
+	}
+}
