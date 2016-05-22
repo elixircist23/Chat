85621a66393a94cff3787f1c7863f0cc67e379cb
elixircist23 <elixircist@gmail.com>
2016-05-22 15:26:34 -0400

    dat system.exit(0);

--- ChatClient.java
+++ ChatClient.java
@@ -2,10 +2,13 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-public class ChatClient {
+public class ChatClient implements Runnable{
 	
 	public static Socket clientSocket = null;
 	public static PrintStream out = null;
+	public static ObjectOutputStream oos = null;
+	public static DataInputStream in = null;
+	public static ObjectInputStream ois = null;
 	public static Scanner stdin = new Scanner(System.in);
 	public static boolean closed = false;
 
@@ -15,6 +18,9 @@ public class ChatClient {
 			
 			clientSocket = new Socket("localhost", 1500);
 			out = new PrintStream(clientSocket.getOutputStream());
+			oos = new ObjectOutputStream(out);
+			in = new DataInputStream(clientSocket.getInputStream());
+			ois = new ObjectInputStream(in);
 			
 		}catch(Exception e){e.printStackTrace();}
 		
@@ -23,25 +29,53 @@ public class ChatClient {
 		if(clientSocket != null && out != null){
 			
 			try{
-						
+				
+				new Thread(new ChatClient()).start();
+				
 				while(!closed){
 				
 					String input = stdin.next();
 				
-					out.println(input);
-				
 					if(input.equals("quit")){
+						Quit q = new Quit();
+						oos.writeObject(q);
+						closed = true;
 						break;
 					}
-				
+					else{
+						TempMessage msg = new TempMessage(input);
+						oos.writeObject(msg);
+					}
+									
 				}
 				
 				out.close();
+				oos.close();
+				in.close();
+				ois.close();
 				clientSocket.close();
+				System.exit(0);
 			
 			}catch(Exception e){e.printStackTrace();}
 			
 		}
+				
+	}
+	
+	public void run(){
+		
+		Object responseLine;
+		
+		try{
+						
+			while((responseLine = ois.readObject())!= null){
+				System.out.println(responseLine.getClass().getName());
+				if(responseLine.getClass().getName().equals("Quit")){
+					break;
+				}
+			}
+			
+		}catch(Exception e){e.printStackTrace();}
 		
 	}
 
