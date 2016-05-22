3b55bb2d1859a92b53d7e84b4645aeddc3da6ab1
elixircist23 <elixircist@gmail.com>
2016-05-22 00:16:37 -0400

    fix in output

--- Client.java
+++ Client.java
@@ -2,71 +2,75 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-public class Client{
-	public static void main(String[] args){
+public class Client implements Runnable{
+	
+	private static Socket clientSocket = null;
+	private static InputStream in = null;
+	private static ObjectInputStream objectInStream = null;
+	private static OutputStream out = null;
+	private static ObjectOutputStream objectOutStream = null;
+	private static BufferedReader inputLine = null;
+	private static boolean closed = false;
+	
+	
+	public static void main(String[] args) {
 		
-		String hostname = "192.168.1.7";
-		//String hostname = "localhost";
+		String hostname = "localhost";
 		int port = 1500;
 		
-		Socket clientSocket = null;
-		InputStream in = null;
-		ObjectInputStream objectInStream = null;
-		OutputStream out = null;
-		ObjectOutputStream objectOutStream = null;
 		
 		try{
 			clientSocket = new Socket(hostname, port);
+			inputLine = new BufferedReader(new InputStreamReader(System.in));
 			in = clientSocket.getInputStream();
 			out = clientSocket.getOutputStream();
+			objectInStream = new ObjectInputStream(in);
+			objectOutStream = new ObjectOutputStream(out);
+			
 			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		
+		if(clientSocket != null && out != null && in != null){
 			
-		try{
-			while(true){
+			try{
 				
-				System.out.println("in client loop");
-				
-				objectOutStream = new ObjectOutputStream(out);
-				
-				Scanner stdin = new Scanner(System.in);
-				String userIn = stdin.next();
-				
-				switch(userIn){
-					case "m":
-						TempMessage tmp = new TempMessage("hello");
-						objectOutStream.writeObject(tmp);
-						break;
-				
-					case "q":
-						Quit q = new Quit();
-						objectOutStream.writeObject(q);
-						break;
+				new Thread(new Client()).start();
+				while(!closed){
+					objectOutStream.writeObject(new TempMessage(inputLine.toString()));
 				}
 				
-				if(userIn.equals("q")){
+				out.close();
+				in.close();
+				objectOutStream.close();
+				objectInStream.close();
+				clientSocket.close();
+			
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+		
+	public void run(){
+		String line = null;
+		try{
+			line = objectInStream.readObject().getClass().getName();
+		}catch(Exception e1){e1.printStackTrace();}
+		
+		try{
+			while(line != null){
+				System.out.println(line);
+				if(line.equals("Quit")){
 					break;
 				}
-				
-				
-				objectInStream = new ObjectInputStream(in);
-				System.out.println(objectInStream.readObject());
-							
-				
 			}
-			
-			//in.close();
-			//out.close();
-			//objectInStream.close();
-			//objectOutStream.close();
-			//clientSocket.close();
-			
+			closed = true;
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		
-		
 	}
+	
 }
